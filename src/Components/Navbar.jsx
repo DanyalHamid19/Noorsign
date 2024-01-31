5857b13d75c8e31159249e2a1a5e6267fb0b266f
Danyal Hamid <138367664+DanyalHamid19@users.noreply.github.com>
2024-01-31 17:02:15 +0500

    navbar setting

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -35,6 +35,19 @@ const Navbar = () => {
             </span>
           </Link>
 
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto">
               <li className="nav-item">
